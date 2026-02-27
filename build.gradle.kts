5b70047d44f96199e6d959d0b349873affa6a8ee
Jose Rodriguez Ballester <joserodriguezballester@github.com>
2026-02-27 16:27:51 +0100

    Actualizado Kotlin a 1.9.20 para compatibilidad con Compose Compiler 1.5.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 plugins {
     id("com.android.application") version "8.1.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
-    id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.20" apply false
+    id("com.google.devtools.ksp") version "1.9.20-1.0.14" apply false
 }
 
 buildscript {
-    extra["kotlin_version"] = "1.9.0"
+    extra["kotlin_version"] = "1.9.20"
 }
