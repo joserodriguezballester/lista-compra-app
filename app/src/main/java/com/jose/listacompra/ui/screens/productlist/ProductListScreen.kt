3b908c6964320f7ad4fe1f13ebdf84dd5ad76044
Debian <admin@ip-172-31-39-199>
2026-04-07 16:29:41 +0200

    fix: añadir import Uri en ProductListScreen

--- app/src/main/java/com/jose/listacompra/ui/screens/productlist/ProductListScreen.kt
+++ app/src/main/java/com/jose/listacompra/ui/screens/productlist/ProductListScreen.kt
@@ -1,5 +1,6 @@
 package com.jose.listacompra.ui.screens.productlist
 
+import android.net.Uri
 import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
