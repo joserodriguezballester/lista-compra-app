1b08d86f40caec40771a9c79c0a7b1b3babe251d
Debian <admin@ip-172-31-39-199>
2026-04-09 16:02:50 +0200

    feat: Añadir artículos del ticket a la semilla
    - 19 artículos nuevos del ticket
    - Categorías: Aceites, Limpieza, Bebidas, Aperitivos, Lácteos, Quesos, Postres, Pizza, Salsas, Pasta
    - Con EAN, precio y URL de imagen de Carrefour

--- app/src/main/java/com/jose/listacompra/data/local/dataseeder/articulosBase.kt
+++ app/src/main/java/com/jose/listacompra/data/local/dataseeder/articulosBase.kt
@@ -1,6 +1,61 @@
 package com.jose.listacompra.data.local.dataseeder
 
  val articulosBase = listOf(
+    // === ARTÍCULOS DEL TICKET (2026-04-09) ===
+    
+    // Aceites y Grasas
+    SeedArticulo("Aceite de Girasol 5L", 23, 10.75f, 5f, "L", "8410100033833",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100033833_01.jpg"),
+
+    // Limpieza e Higiene
+    SeedArticulo("Fairy Poder 900ML", 36, 5.39f, 900f, "ml", "8006540833162",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8006540833162_01.jpg"),
+    SeedArticulo("Papel Aluminio 50M", 36, 7.99f, 50f, "m", "8410100021052",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100021052_01.jpg"),
+    SeedArticulo("Papel Cocina Foxy", 36, 2.75f, 3f, "ud", "8004260492143",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8004260492143_01.jpg"),
+    SeedArticulo("Recambios Maquinilla", 35, 3.45f, 1f, "ud", null, null),
+
+    // Bebidas y Café
+    SeedArticulo("Cafe Cortado Nescafé", 3, 8.35f, 16f, "ud", "7613032431442",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/7613032431442_01.jpg"),
+    SeedArticulo("Cafe Capuccino 250ML", 3, 0.79f, 250f, "ml", "8410100055262",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100055262_01.jpg"),
+
+    // Aperitivos y Snacks
+    SeedArticulo("Cheetos Pelotazos", 29, 1.89f, 130f, "g", "8410199003311",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410199003311_01.jpg"),
+    SeedArticulo("Drakis Pandilla 75G", 29, 1.89f, 75f, "g", "8410199005391",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410199005391_01.jpg"),
+    SeedArticulo("Patatas Onduladas 170", 29, 0.89f, 170f, "g", "8410100010015",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100010015_01.jpg"),
+
+    // Lácteos, Quesos y Postres
+    SeedArticulo("Leche Semidesnatada", 11, 0.88f, 1f, "L", "8410100010015",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100010015_01.jpg"),
+    SeedArticulo("Mozzarella Carrefour", 8, 0.89f, 125f, "g", "3245412151676",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/3245412151676_01.jpg"),
+    SeedArticulo("Queso Tierno Lonchas", 8, 1.95f, 1f, "ud", "8410103529364",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410103529364_01.jpg"),
+    SeedArticulo("Flan de Café 4X100", 12, 2.69f, 400f, "g", "8410100052308",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100052308_01.jpg"),
+    SeedArticulo("Natillas Vainilla X4", 12, 0.99f, 400f, "g", "8410100021656",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100021656_01.jpg"),
+
+    // Platos Preparados y Pizza
+    SeedArticulo("Pizza Campofrio 365G", 26, 3.19f, 365f, "g", "8410510340352",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410510340352_01.jpg"),
+    SeedArticulo("Tortilla Palacios", 21, 5.95f, 650f, "g", "8410920002161",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410920002161_01.jpg"),
+    SeedArticulo("Salsa Alioli Chovi", 28, 3.15f, 250f, "ml", "8410558000300",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410558000300_01.jpg"),
+
+    // Despensa (Pasta, Conservas, Legumbres)
+    SeedArticulo("Fideua Carrefour 500", 24, 0.80f, 500f, "g", "8410100015501",
+        "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100015501_01.jpg"),
+
+    // === ARTÍCULOS BASE ORIGINALES ===
+
     SeedArticulo("Leche Entera Carrefour", 15, 1.15f, 1f, "L", "8410100010015",
         "https://jcarrefour.vtexassets.com/arquivos/ids/2202611/8410100010015_01.jpg"),
 
